5122fdfaf0bbfa9ea3451414447bcd5f223909d7
yalunwu <yalunwu@gmail.com>
2014-04-21 11:17:20 +0200

    Added in decode and execute, Updated Fetch
    -updated  fetch
    -added decode and execute(not complete)
    -updated gitignore to remove files from quartus and modelsim

---
+++ execute.vhd
@@ -0,0 +1,53 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity execute is
+  port (
+	clock
+  ) ;
+end entity ; -- Execute
+
+
+architecture arch of execute is
+
+	signal 
+	constant NOP :		std_logic_vector(4 downto 0) :="00000";
+	constant MOV :		std_logic_vector(4 downto 0) :="00001";
+	constant ADD :		std_logic_vector(4 downto 0) :="00010";
+	constant ADDI:		std_logic_vector(4 downto 0) :="00011";
+
+	constant INC :		std_logic_vector(4 downto 0) :="00100";
+	constant SUB :		std_logic_vector(4 downto 0) :="00101";
+	constant SUBI:		std_logic_vector(4 downto 0) :="00110";
+	constant DEC :		std_logic_vector(4 downto 0) :="00111";
+
+	constant MUT :		std_logic_vector(4 downto 0) :="01000";
+	constant MUTI:		std_logic_vector(4 downto 0) :="01001";
+	constant DIV :		std_logic_vector(4 downto 0) :="01010";
+	constant DIVI:		std_logic_vector(4 downto 0) :="01011";
+
+	constant SHL :		std_logic_vector(4 downto 0) :="01100"; --sla
+	constant SHR :		std_logic_vector(4 downto 0) :="01101";	--sra
+	constant AN  :		std_logic_vector(4 downto 0) :="01110";	--AND
+	constant ANDI:		std_logic_vector(4 downto 0) :="01111";	
+
+	constant O 	 :		std_logic_vector(4 downto 0) :="10000";--or
+	constant ORI :		std_logic_vector(4 downto 0) :="10001";
+	constant NO  :		std_logic_vector(4 downto 0) :="10010";--nor
+	constant NT  :		std_logic_vector(4 downto 0) :="10011";--not
+
+	constant XO  :		std_logic_vector(4 downto 0) :="10100";--xor
+	constant LOD :		std_logic_vector(4 downto 0) :="10101";
+	constant STE :		std_logic_vector(4 downto 0) :="10110";
+	constant WRT :		std_logic_vector(4 downto 0) :="10111";
+
+	constant SWP :		std_logic_vector(4 downto 0) :="11000";
+	constant BRH :		std_logic_vector(4 downto 0) :="11001";
+	constant BEQ :		std_logic_vector(4 downto 0) :="11010";
+	constant BNE :		std_logic_vector(4 downto 0) :="11011";
+	constant INT :		std_logic_vector(4 downto 0) :="11100";
+
+begin
+
+end architecture ; -- arch
--- fetch.vhd
+++ fetch.vhd
@@ -7,45 +7,51 @@ entity fetch is
 			clock: 				in 	std_logic;
 			reset:	 			in 	std_logic;
 			readIn:				in 	std_logic_vector(31 downto 0);
-			programCounter:		inout std_logic_vector(31 downto 0):=std_logic_vector(to_unsigned(0,32));
+			EnabledFetch:		in 	std_logic;
+			Branched:			in  std_logic;
+			BranchLocation:		in  std_logic_vector(31 downto 0);
+			programCounter:		out std_logic_vector(31 downto 0):=std_logic_vector(to_unsigned(0,32));
 			nextInstruction:	out std_logic_vector(31 downto 0):=std_logic_vector(to_unsigned(0,32))
   ) ;
 end entity ; -- Fetch
 
-architecture  of fetch is
+architecture arch of fetch is
 
-	signal			temp
-	signal 			state:				std_logic_vector(2 downto 0):='111';
-	constant		waiting:			std_logic_vector(2 downto 0):='000';
-	constant		resetMode:			std_logic_vector(2 downto 0):='111';
-	constant		readIn:				std_logic_vector(2 downto 0):='001';
-	constant		readWait:			std_logic_vector(2 downto 0):='010';
+	signal			counter:			std_logic_vector(31 downto 0);
+	signal 			state:				std_logic_vector(2 downto 0):="111";		
+	constant		waiting:			std_logic_vector(2 downto 0):="000";
+	constant		resetMode:			std_logic_vector(2 downto 0):="111";
+	constant		reading:			std_logic_vector(2 downto 0):="001";
+	constant		readWait:			std_logic_vector(2 downto 0):="010";
+	constant		Branching:			std_logic_vector(2 downto 0):="100";
+	type 			Cache 				is array (15 downto 0 ) of std_logic_vector(31 downto 0);
+	signal			instructionCache: 	Cache 						:=((others=> (others=>'0')));
 
 begin
+	
 
 	process(clock,reset)
 	begin
 		if rising_edge(clock) then
-			case( state ) is
-				when waiting =>
-					if reset = '1' then
-						state <= resetMode;
-					elsif readIn = '1' then
-						state <= readIn;
-					end if ;
-
-				when resetMode =>
-					programCounter <= std_logic_vector(to_unsigned(0,32));
-					nextInstruction <=std_logic_vector(to_unsigned(0,32));
-				when readIn =>
-					nextInstruction <=readIn;				
 			
-				when others =>
-				state <=resetMode;
 
-			
-			end case ;
-			programCounter = programCounter +1;
+			if reset = '1' then
+				counter <= std_logic_vector(to_unsigned(0,32));
+				programCounter <= std_logic_vector(to_unsigned(0,32));
+				nextInstruction <=std_logic_vector(to_unsigned(0,32));
+			elsif Branched = '1' then
+				counter <=BranchLocation;
+				programCounter <= BranchLocation;
+				nextInstruction <=std_logic_vector(to_unsigned(0,32));
+			elsif EnabledFetch ='1' then
+				programCounter <= std_logic_vector(unsigned(counter)+to_unsigned(1,32));
+				counter <= std_logic_vector(unsigned(counter)+to_unsigned(1,32));
+				nextInstruction	<= instructionCache(0);
+				instructionCache(14 downto 0) <= instructionCache(15 downto 1);
+				instructionCache(15) <= readIn;
+					
+			end if ;
+
 		end if ;
 	end process ; -- 
 end architecture ; -- 
